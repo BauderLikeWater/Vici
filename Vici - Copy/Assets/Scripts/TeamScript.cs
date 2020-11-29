d5bbde4abadf2ecf4127ab08318ac7cff5ff6625
BauderLikeWater <bauderlikewater@gmail.com>
2020-11-29 16:02:35 -0600

    Added Outline Script

--- Vici - Copy/Assets/Scripts/TeamScript.cs
+++ Vici - Copy/Assets/Scripts/TeamScript.cs
@@ -19,7 +19,7 @@ public class TeamScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        DontDestroyOnLoad(this.gameObject);
+        DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
